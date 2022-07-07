44225f9c0be0c5e2393c9baf25ecabbee66f7034
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-07-07 17:54:15 +0300

    finishing authentification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyALlH1Wv_5Vfo9W5uOFVUXWZLNETLaU-qQ",
+    authDomain: "clipz-636b0.firebaseapp.com",
+    projectId: "clipz-636b0",
+    storageBucket: "clipz-636b0.appspot.com",
+    messagingSenderId: "45154035487",
+    appId: "1:45154035487:web:20eacda84425d8ff15f22a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import firebase from "firebase/compat/app";
+
 export const environment = {
-  production: false
+  production: true,
+    firebase : {
+      apiKey: "AIzaSyALlH1Wv_5Vfo9W5uOFVUXWZLNETLaU-qQ",
+      authDomain: "clipz-636b0.firebaseapp.com",
+      projectId: "clipz-636b0",
+      storageBucket: "clipz-636b0.appspot.com",
+      messagingSenderId: "45154035487",
+      appId: "1:45154035487:web:20eacda84425d8ff15f22a"
+    }
 };
 
 /*
